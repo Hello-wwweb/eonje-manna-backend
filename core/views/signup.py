33c2369d5116c14e146a53465d0868292516459d
tjdus <dusml0277@gmail.com>
2024-11-25 21:00:51 +0900

    fix

--- core/views/signup.py
+++ core/views/signup.py
@@ -23,16 +23,16 @@ class SignupView(APIView):
                 username = serializer.validated_data["username"]
                 password = serializer.validated_data["password"]
                 name = serializer.validated_data["name"]
-                email = serializer.validated_data["email"]
                 with transaction.atomic():
-                    user = User.objects.create_user(
-                        username=username, password=password
-                    )
-                    Member.objects.create(user=user, name=name, email=email)
+                email = serializer.validated_data["email"]
+                user = User.objects.create_user(
+                    username=username, password=password
+                )
+                Member.objects.create(user=user, name=name, email=email)
 
-                    return Response(
-                        {"message": "Signup successful"}, status=status.HTTP_200_OK
-                    )
+                return Response(
+                    {"message": "Signup successful"}, status=status.HTTP_200_OK
+                )
             except Exception as e:
                 return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
