5e5fd7cd569779e112f5a05ac07bf201409bf131
tjdus <dusml0277@gmail.com>
2024-11-26 14:23:42 +0900

    fix settings

--- eonje_manna_backend/settings.py
+++ eonje_manna_backend/settings.py
@@ -157,4 +157,8 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost", "https://localhost:8000"]
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost',
+    'http://127.0.0.1',
+    'http://0.0.0.0',
+]
