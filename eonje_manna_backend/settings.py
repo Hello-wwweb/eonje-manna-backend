7152e84667f8a2d0981c5ef98ac37fed81c6ecb2
tjdus <dusml0277@gmail.com>
2024-11-26 19:10:56 +0900

    csrf

--- eonje_manna_backend/settings.py
+++ eonje_manna_backend/settings.py
@@ -56,10 +56,6 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 
 ]
-
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = True
-
 ROOT_URLCONF = "eonje_manna_backend.urls"
 
 TEMPLATES = [
