ffb7d2d98f249b40be4d43f63bd2c5b9762a2a47
tjdus <dusml0277@gmail.com>
2024-11-28 00:34:42 +0900

    fix

--- core/serializers/group_member.py
+++ core/serializers/group_member.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 
 class GroupMemberSerializer(serializers.Serializer):
     username = serializers.CharField(source='member.user.username')
-    user_id = serializers.CharField(source='member.user.id')
+    user_id = serializers.IntegerField(source='member.user.id')
     name = serializers.CharField(source='member.name')
     nickname = serializers.CharField()
     email = serializers.CharField(source='member.email')
