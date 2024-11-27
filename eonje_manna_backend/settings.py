e05d95abbc3e2e4de854d37b87692a38743a371f
tjdus <dusml0277@gmail.com>
2024-11-28 02:01:33 +0900

    edit settings

--- eonje_manna_backend/settings.py
+++ eonje_manna_backend/settings.py
@@ -159,8 +159,10 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ORIGIN_ALLOW_ALL=True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    "http://54.180.214.199"
+]
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOW_METHODS = (
