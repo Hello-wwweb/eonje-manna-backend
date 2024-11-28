0f0c7ca59fe9d2612f574eb7a2a81c4d50eac387
tjdus <dusml0277@gmail.com>
2024-11-28 21:25:34 +0900

    fix

--- core/serializers/meeting_group.py
+++ core/serializers/meeting_group.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from core.models import MeetingGroup
 
 class MeetingGroupSerializer(serializers.ModelSerializer):
-    created_by = serializers.IntegerField(read_only=True)
+    created_by = serializers.CharField(read_only=True)
     class Meta:
         model = MeetingGroup
         fields = ['id', 'name', 'description', 'created_by']
