1a2c0c17b50caf4d30e6715a2e6cd86b76b80e6b
tjdus <dusml0277@gmail.com>
2024-11-29 17:18:49 +0900

    fix

--- eonje_manna_backend/settings.py
+++ eonje_manna_backend/settings.py
@@ -55,8 +55,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'corsheaders.middleware.CorsMiddleware',
-
 ]
 ROOT_URLCONF = "eonje_manna_backend.urls"
 
