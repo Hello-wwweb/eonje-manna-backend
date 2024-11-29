7635d298d8488f011ad28a587ccc652109a0ec63
tjdus <dusml0277@gmail.com>
2024-11-29 16:31:10 +0900

    fix

--- core/views/event.py
+++ core/views/event.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from core.models import Event, MeetingGroup
+from core.models import Event, MeetingGroup, Member, Membership
 from core.serializers.event import EventSerializer, \
     EventRequestforPatchSerializer, EventRequestSerializer
 
